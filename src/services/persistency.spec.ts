cfbc1b429827447be69c415b6cf37bc575e9f837
ivnogg <ivnocrypto@gmail.com>
2023-01-15 04:19:50 -0300

    add assercoes com Jest

---
+++ src/services/persistency.spec.ts
@@ -0,0 +1,13 @@
+describe('TESTANDO ALGUMA COISA 🧪', () => {
+  it('descrição do teste (IT)', () => {
+    const number = 1;
+    expect(number).toBe(1);
+  });
+});
+
+describe('TESTANDO OUTRA COISA 🧪', () => {
+  test('descrição de teste (TESTE)', () => {
+    const nome = 'Ivano';
+    expect(nome).toBe('Ivano');
+  });
+});
